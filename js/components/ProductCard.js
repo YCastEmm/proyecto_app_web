1ce17eeef09ae1effa17901b0ecc71fe3a29d069
ycastagnola@x-28.com <ycastagnola@x-28.com>
2025-06-10 22:52:43 -0300

    Ajustes generales y de estilo

--- js/components/ProductCard.js
+++ js/components/ProductCard.js
@@ -2,14 +2,14 @@
 // se usa en render.js para generar el contenido de cada producto
 // se exporta la funcion createProductCard que toma un objeto producto como parametro
 
-function createProductCard(producto){
-    return `<div class="card" style="width: 18rem;">
-                <img src="${producto.image}" class="card-img-top" alt="">
-                <div class="card-body">
+
+export function createProductCard(producto){
+    return `<div class="card text-center" style="width: 18rem;">
+                <img src="${producto.image}" class="card-img-top img-thumbnail card-image" alt="">
+                <div class="card-body d-flex flex-column justify-content-between">
                     <h5 class="card-title">${producto.title}</h5>
-                    <h5 class="price">${producto.price}</h5>
-                    <p class="card-text">${producto.description}</p>
-                    <a href="#" class="btn btn-primary">VER MÁS</a>
+                    <h5 class="price">$${producto.price}</h5>
+                    <a href="#" class="btn btn-primary card-btn">VER MÁS</a>
                 </div>
             </div>
 `

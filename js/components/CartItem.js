9ae50464020337d92bace2ab159c7745bee9480c
ycastagnola@x-28.com <ycastagnola@x-28.com>
2025-06-11 20:48:32 -0300

    Movida la logica de crear los items del carrito a CartItem

---
+++ js/components/CartItem.js
@@ -0,0 +1,19 @@
+// Retorna el html de cada item del carrito
+export const createCartItem = (cartItem) => {
+    return `
+                <div class="d-flex justify-content-between align-items-start mb-4">
+                    <div>
+                        <div class="fw-semibold">${cartItem.title}</div>
+                        <div class="d-flex align-items-center mt-2">
+                            <button class="btn btn-cantidad btn-restar" data-id="${cartItem.id}">-</button>
+                            <span class="cantidad fw-bold text-center mx-2">${cartItem.cantidad}</span>
+                            <button class="btn btn-cantidad btn-sumar" data-id="${cartItem.id}">+</button>
+                        </div>
+                    </div>
+                    <div class="text-end">
+                        <div class="fw-bold">$${(cartItem.price * cartItem.cantidad).toFixed(2)}</div>
+                        <button class="btn btn-lg text-danger mt-1 p-0 btn-eliminar" data-id="${cartItem.id}"><i class="bi bi-trash"></i></button>
+                    </div>
+                </div>
+            `
+} 

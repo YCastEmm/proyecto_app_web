b5055d5d158fd7cb8a3caee7ea6653f9185431b4
Carla Lopez <lpz.carla12@gmail.com>
2025-06-22 17:38:26 -0300

    arreglar: función updateBadgeCounter

--- js/components/BadgeCounter.js
+++ js/components/BadgeCounter.js
@@ -1,25 +1,29 @@
 import { getCartFromLS } from "../cart/localStorageHandler.js";
 
-function createBadgeCounter(contadorTotal){
-    return `<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-${contadorTotal}
-            </span>`
-}
-
-
-
+//   function createBadgeCounter(contadorTotal){
+//       return `<span id="contadorTotal" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger hidden">
+//   ${contadorTotal}
+//               </span>`
+//   }
 
 export function updateBadgeCounter() {
 
     let contadorTotal = 0
     const carrito = getCartFromLS()
     carrito.forEach(element => {
-        contadorTotal += element.cantidad 
+        contadorTotal += element.cantidad  
+    })
+    
+    
 
-    });
+    if (contadorTotal < 1){
+        const removeCounter = document.getElementById("contadorTotal")
+        removeCounter.remove("contadorTotal");
+    } else {
+        const contadorTotalContenedor = document.getElementById("contadorTotal")
+        contadorTotalContenedor.innerHTML = contadorTotal            
 
-    const contadorTotalContenedor = document.getElementById("contadorTotal")
-    contadorTotalContenedor.innerHTML = contadorTotal    
-
+    }
+   
 }
 
